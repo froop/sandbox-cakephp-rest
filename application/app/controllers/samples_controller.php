a238f22ce84c909de06b4b000f2494131f96257a
froop <frooprogrammer@gmail.com>
2013-04-04 15:07:59 +0900

    add Model#find() fields option

--- application/app/controllers/samples_controller.php
+++ application/app/controllers/samples_controller.php
@@ -6,12 +6,12 @@ class SamplesController extends AppController {
 
 	function index() {
 		$output->sample = array(
-			'key1' => 'value1',
-			'key2' => array(
-				array('key21' => 'value21', 'key22' => 21),
-				array('key21' => 'value22', 'key22' => 22)
-			),
-			'key3' => $this->Sample->find('all')
+				'key1' => 'value1',
+				'key2' => array(
+						array('key21' => 'value21', 'key22' => 21),
+						array('key21' => 'value22', 'key22' => 22)),
+				'key3' => $this->Sample->find('all', array(
+						'fields' => array('id', 'text1', 'date1', 'number1')))
 		);
 		$this->set('output', $output);
 	}
