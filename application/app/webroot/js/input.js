56078be51d816881038a00682d806dc07f451256
froop <frooprogrammer@gmail.com>
2013-04-08 10:31:44 +0900

    extract input.js

---
+++ application/app/webroot/js/input.js
@@ -0,0 +1,37 @@
+$(function () {
+	"use strict";
+	var HTTP_BAD_REQUEST = 400;
+	var id = $.url().param()["id"];
+
+	if (id) {
+		$.getJSON("../samples/" + id + ".json", function (data) {
+			$("input[name=text1]").val(data.text1);
+		});
+	}
+
+	$("#form1").on("submit", function () {
+		var url = "../samples" + (id ? "/" + id : "");
+		$.ajax(url, {
+			type : "POST",
+			data : $("#form1").serialize(),
+			success : function (responseText) {
+				$("#message").text(responseText);
+			},
+			error : function(xhr, textStatus, errorThrown) {
+				if (xhr.status === HTTP_BAD_REQUEST) {
+					$("#message").text(xhr.responseText);
+				} else {
+					alert(xhr.status + ":" + textStatus + ":" + errorThrown);
+				}
+			},
+			complete : function () {
+				$("#back").show();
+			}
+		});
+
+		// デフォルト動作抑止
+		return false;
+	});
+
+	$("#back").hide();
+});
