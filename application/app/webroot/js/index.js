709ea8a7a6da30e18d2b8afbbe47b713d497e30a
froop <frooprogrammer@gmail.com>
2013-04-08 16:29:41 +0900

    refactoring $link

--- application/app/webroot/js/index.js
+++ application/app/webroot/js/index.js
@@ -7,12 +7,10 @@ $(function () {
 		$.each(data.list, function () {
 			var url = "input.html?" + $.param({ id : this.Sample.id });
 			var text = this.Sample.modified + ":" + this.Sample.text1;
-			var $item = $("<li>");
-
-			$("<a>").attr("href", url)
-					.text(text)
-					.appendTo($item);
-			$item.appendTo($("#list1"));
+			var $link = $("<a>")
+					.attr("href", url)
+					.text(text);
+			$("#list1").append($("<li>").append($link));
 		});
 	});
 });
