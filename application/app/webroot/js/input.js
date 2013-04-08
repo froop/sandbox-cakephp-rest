424e77da0d5e3bf2cda1d85d6f17e7b10a3f8bdb
froop <frooprogrammer@gmail.com>
2013-04-08 16:52:38 +0900

    add GET xhr.status === HTTP_NOT_FOUND

--- application/app/webroot/js/input.js
+++ application/app/webroot/js/input.js
@@ -4,6 +4,7 @@ $(function () {
 	var HTTP_BAD_REQUEST = 400;
 	var HTTP_NOT_FOUND = 404;
 	var id = $.url().param()["id"];
+	var $message = $("#message");
 
 	if (id) {
 		$.ajax({
@@ -14,14 +15,17 @@ $(function () {
 				$("input[name=text1]").val(data.text1);
 			},
 			error : function(xhr, textStatus, errorThrown) {
-				alert("[id=" + id + "]" + xhr.status + ":" + textStatus + ":"
-						+ errorThrown);
+				if (xhr.status === HTTP_NOT_FOUND) {
+					$message.text("ID is not found");
+				} else {
+					alert("[id=" + id + "]" + xhr.status + ":" + textStatus
+							+ ":" + errorThrown);
+				}
 			},
 		});
 	}
 
 	$("#form1").on("submit", function () {
-		var $message = $("#message");
 		$.ajax(BASE_URL + (id ? "/" + id : ""), {
 			type : "POST",
 			data : $("#form1").serialize(),
