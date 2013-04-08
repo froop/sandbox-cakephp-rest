b927844d18fdc1483fd46d4aa91cc15347488f4f
froop <frooprogrammer@gmail.com>
2013-04-08 16:26:43 +0900

    extract url

--- application/app/webroot/js/index.js
+++ application/app/webroot/js/index.js
@@ -5,12 +5,11 @@ $(function () {
 		$("#id1").text(data.key1);
 
 		$.each(data.list, function () {
+			var url = "input.html?" + $.param({ id : this.Sample.id });
 			var text = this.Sample.modified + ":" + this.Sample.text1;
 			var $item = $("<li>");
 
-			$("<a>").attr("href", "input.html?" + $.param({
-						id : this.Sample.id
-					}))
+			$("<a>").attr("href", url)
 					.text(text)
 					.appendTo($item);
 			$item.appendTo($("#list1"));
