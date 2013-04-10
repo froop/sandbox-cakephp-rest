867c6b03b89e4bb88b3658b9625e9e50d59b341c
froop <frooprogrammer@gmail.com>
2013-04-10 10:30:02 +0900

    add beforeSend() template

--- application/app/webroot/js/index.js
+++ application/app/webroot/js/index.js
@@ -5,9 +5,13 @@ $(function () {
 		url : "../api/samples.json",
 		type : "GET",
 		dataType : "json",
+		beforeSend : function(xhr) {
+//			xhr.setRequestHeader("If-Modified-Since",
+//					"Thu, 01 Jun 1970 00:00:00 GMT");
+		},
 		success : function (data) {
 			$("#id1").text(data.key1);
-	
+
 			$.each(data.list, function () {
 				var $link = $("<a>")
 						.attr("href", "input.html?" + $.param({ id : this.Sample.id }))
