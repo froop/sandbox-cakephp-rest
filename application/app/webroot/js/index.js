50417adad205bb4e83f857faffc16a7a1bb379f7
froop <frooprogrammer@gmail.com>
2013-04-10 10:17:46 +0900

    refactoring use $.ajax() <- $.getJSON()

--- application/app/webroot/js/index.js
+++ application/app/webroot/js/index.js
@@ -1,14 +1,19 @@
 $(function () {
 	"use strict";
 
-	$.getJSON("../api/samples.json", function (data) {
-		$("#id1").text(data.key1);
-
-		$.each(data.list, function () {
-			var $link = $("<a>")
-					.attr("href", "input.html?" + $.param({ id : this.Sample.id }))
-					.text(this.Sample.modified + ":" + this.Sample.text1);
-			$("#list1").append($("<li>").append($link));
-		});
+	$.ajax({
+		url : "../api/samples.json",
+		type : "GET",
+		dataType : "json",
+		success : function (data) {
+			$("#id1").text(data.key1);
+	
+			$.each(data.list, function () {
+				var $link = $("<a>")
+						.attr("href", "input.html?" + $.param({ id : this.Sample.id }))
+						.text(this.Sample.modified + ":" + this.Sample.text1);
+				$("#list1").append($("<li>").append($link));
+			});
+		}
 	});
 });
