643132aa9be49159598c2af966545705f6f39868
froop <frooprogrammer@gmail.com>
2013-04-11 10:34:58 +0900

    move setEmptyBody()

--- application/app/controllers/components/response.php
+++ application/app/controllers/components/response.php
@@ -26,12 +26,12 @@ class ResponseComponent extends Object {
 		$this->setEmptyBody();
 	}
 
+	function setEmptyBody() {
+		$this->controller->render('/commons/empty');
+	}
+
 	private function setMessageBody($output) {
 		$this->controller->set('output', $output);
 		$this->controller->render('/commons/message');
-	}
-
-	function setEmptyBody() {
-		$this->controller->render('/commons/empty');
 	}
 }
